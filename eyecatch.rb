b5eb45f14392bbf510d959b022204cfa9404e587
Takao NAITO <xarsh@outlook.com>
2016-01-13 04:07:11 +0900

    Update Eyecatch conf.

--- eyecatch.rb
+++ eyecatch.rb
@@ -8,7 +8,7 @@ serve 'node server.js'
 port 3000
 
 route('/login') {
-  fill_in "username", "jack"
-  fill_in "password", "secret"
+  fill_in "username", with: "jack"
+  fill_in "password", with: "secret"
   click_button "Submit"
 }
